a5da8b9e6e33b03bd4b9c8a8ea42045755033d44
minesettimi <minesettimi@gmail.com>
2026-08-29 18:47:31 -0400

    initial

---
+++ Plugin.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Reflection;
+using BepInEx;
+using EFT.Interactive;
+using HarmonyLib;
+using SPT.Reflection.Patching;
+
+namespace PMCCoop
+{
+    [BepInPlugin("com.minesettimi.pmccoop", "PMCCoop", "1.0.0")]
+    public class Plugin : BaseUnityPlugin
+    {
+        private void Awake()
+        {
+            new ScavCooperationPatch().Enable();
+        }
+    }
+
+    public class ScavCooperationPatch : ModulePatch
+    {
+        protected override MethodBase GetTargetMethod()
+        {
+            return AccessTools.Method(typeof(ScavCooperationRequirement),
+                nameof(ScavCooperationRequirement.UpdateStatus));
+        }
+        
+        [PatchPrefix]
+        public static bool Prefix(ExfiltrationPoint point)
+        {
+            point.SetStatusLogged(point.Entered.Count > 1 ? EExfiltrationStatus.Countdown : EExfiltrationStatus.UncompleteRequirements, "CooperationRequirement");
+            return false;
+        }
+    }
+}
