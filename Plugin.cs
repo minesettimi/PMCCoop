9ca478c6d6402a2dedca2393e76b222be0e536d4
minesettimi <minesettimi@gmail.com>
2026-08-29 22:24:58 -0400

    permanently active

--- Plugin.cs
+++ Plugin.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Reflection;
+﻿using System.Reflection;
 using BepInEx;
 using EFT.Interactive;
 using HarmonyLib;
@@ -13,6 +12,7 @@ namespace PMCCoop
         private void Awake()
         {
             new ScavCooperationPatch().Enable();
+            new ScavCooperationMetPatch().Enable();
         }
     }
 
@@ -27,7 +27,26 @@ namespace PMCCoop
         [PatchPrefix]
         public static bool Prefix(ExfiltrationPoint point)
         {
-            point.SetStatusLogged(point.Entered.Count > 1 ? EExfiltrationStatus.Countdown : EExfiltrationStatus.UncompleteRequirements, "CooperationRequirement");
+            //vanilla scav spot absolutely sucks and can flip on and off seemingly at random, if they start it, let them keep it, this isn't PVP
+            if (point._status == EExfiltrationStatus.RegularMode)
+                return false;
+            
+            point.SetStatusLogged(point.Entered.Count > 1 ? EExfiltrationStatus.RegularMode : EExfiltrationStatus.UncompleteRequirements, "CooperationRequirement");
+            return false;
+        }
+    }
+    
+    public class ScavCooperationMetPatch : ModulePatch
+    {
+        protected override MethodBase GetTargetMethod()
+        {
+            return AccessTools.Method(typeof(ScavCooperationRequirement), nameof(ScavCooperationRequirement.Met));
+        }
+
+        [PatchPrefix]
+        public static bool Prefix(ExfiltrationPoint point, ref bool __result)
+        {
+            __result = point.Status == EExfiltrationStatus.RegularMode;
             return false;
         }
     }
