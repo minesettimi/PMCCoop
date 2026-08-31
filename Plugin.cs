9582b18f25744379dfd6fb3824f4640c19bf5cd7
minesettimi <minesettimi@gmail.com>
2026-08-31 19:14:49 -0400

    debug

--- Plugin.cs
+++ Plugin.cs
@@ -1,9 +1,15 @@
 ﻿using System.Reflection;
 using BepInEx;
 using BepInEx.Configuration;
+using BepInEx.Logging;
+using Comfort.Common;
+using CommonAssets.Scripts.Game;
+using EFT;
 using EFT.Interactive;
+using EFT.Vehicle;
 using HarmonyLib;
 using SPT.Reflection.Patching;
+using UnityEngine;
 
 namespace UniversalCoopExfil
 {
@@ -11,11 +17,15 @@ namespace UniversalCoopExfil
     public class Plugin : BaseUnityPlugin
     {
         public static ConfigEntry<bool> StickyAccess = null!;
+        public static PatchManager PatchManager = null!;
+        public static ManualLogSource PluginLogger = null!;
         
         private void Awake()
         {
-            new ScavCooperationPatch().Enable();
-            new ScavCooperationMetPatch().Enable();
+            PatchManager = new PatchManager(this, true);
+            PatchManager.EnablePatches();
+
+            PluginLogger = Logger;
 
             StickyAccess = Config.Bind("Coop Exfil Settings", "Sticky Access", true,
                 "If enabled, the exfil point will stay open after the conditions are first met.");
@@ -70,6 +80,52 @@ namespace UniversalCoopExfil
                 return true;
             
             __result = point.Status == EExfiltrationStatus.RegularMode;
+            return false;
+        }
+    }
+
+    public class ExfiltrationPointEnterPatch : ModulePatch
+    {
+        protected override MethodBase GetTargetMethod()
+        {
+            return AccessTools.Method(typeof(ExfiltrationPoint), "IPhysicsTrigger.OnTriggerEnter");
+        }
+
+        [PatchPrefix]
+        public static bool Prefix(Collider col, ExfiltrationPoint __instance)
+        {
+            GameWorld instance = Singleton<GameWorld>.Instance;
+            Player? playerByCollider = instance.GetPlayerByCollider(col);
+            if (playerByCollider == null)
+            {
+                Plugin.PluginLogger.LogError("Failed to get player by collider.");
+                return false;
+            }
+
+            if ((instance.BtrController != null && instance.BtrController.BtrVehicle != null && instance.BtrController.BtrVehicle.IsPassenger(playerByCollider, out BTRPassenger _)) || playerByCollider.BtrState == EPlayerBtrState.Inside)
+            {
+                Plugin.PluginLogger.LogError("Player is in BTR.");
+                return false;
+            }
+            
+            if (ExfiltrationController.Instance.BannedPlayers.Contains(playerByCollider.Id))
+            {
+                Plugin.PluginLogger.LogError("Player is banned.");
+                return false;
+            }
+            if (!__instance.InfiltrationMatch(playerByCollider))
+            {
+                Plugin.PluginLogger.LogError("Player doesn't have infiltration match.");
+                return false;
+            }
+            if (__instance.Entered.Contains(playerByCollider))
+            {
+                Plugin.PluginLogger.LogError("Player has already entered point.");
+                return false;
+            }
+            __instance.Entered.Add(playerByCollider);
+            __instance.Proceed(playerByCollider, false);
+
             return false;
         }
     }
